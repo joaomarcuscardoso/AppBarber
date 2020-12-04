88ee7909cc85c93faae473eabf4f8fc1880c2171
joaomarcuscardoso <rfcjoujou@gmail.com>
2020-12-04 20:28:49 -0300

    Home Page is in the process of styling

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,12 +1,54 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {useNavigation} from '@react-navigation/native';
 
-import { Text } from 'react-native';
-import { Container } from './styles';
+import { 
+  Container, 
+  Scroller,
+
+  HeaderArea,
+  HeaderTitle,
+  SearchButton,
+
+  LocationArea,
+  LocationInput,
+  LocationFinder,
+
+ } from './styles';
+
+import SearchIcon from '../../assets/search.svg';
+import MyLocationIcon from '../../assets/my_location.svg';
+
 
 export default () => {
+
+  const navigation = useNavigation();
+
+  const [locationText, setLocationText] = useState("");
+
   return (
     <Container>
-      <Text>Home</Text>
+      <Scroller>
+        <HeaderArea>
+          <HeaderTitle numberOfLines={2}>Encontre seu barbeiro favorito</HeaderTitle>
+          <SearchButton onPress={()=>navigation.navigate("Search")} >
+            <SearchIcon width="26" height="26" fill="#ffffff" />
+          </SearchButton>
+        </HeaderArea>
+        <LocationArea>
+          <LocationInput 
+            placeholder="Onde você está?"
+            placeholderTextColor="#ffffff"
+            value={locationText}
+            onChangeText={t=>setLocationText(t)}
+          />
+            <LocationFinder>
+              <MyLocationIcon width="24" height="24" fill="#ffffff" />
+            </LocationFinder>
+        </LocationArea>
+
+
+
+      </Scroller>
     </Container>
   );
 }
