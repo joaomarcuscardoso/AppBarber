270dd0e105b18f9b07b13b124648bf69c00c136f
joaomarcuscardoso <rfcjoujou@gmail.com>
2020-12-07 15:33:32 -0300

    Was done page search

--- src/screens/Search/index.js
+++ src/screens/Search/index.js
@@ -1,12 +1,76 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { 
+  Container,
+  SearchArea,
+  SearchInput,
+  Scroller,
+  LoadingIcon,
+  ListArea,
+  EmptyWarning
 
-import { Text } from 'react-native';
-import { Container } from './styles';
+} from './styles';
+
+import BarberItem from '../../components/BarberItem';
+/* Api */
+import Api from '../../Api';
 
 export default () => {
+
+  const [ searchText, setSearchText] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [emptyList, setEmptyList ] = useState(false);
+  const [list, setList] = useState([]);
+
+  const searchBarbers = async () => {
+    setEmptyList(false);
+    setLoading(true);
+    setList([]);
+
+    if(searchText != "") {
+      let res = await Api.search(searchText);
+      if(res.error == "") {
+        if(res.list.length > 0) {
+          setList(res.list);
+        } else {
+          setEmptyList(true);
+        }
+      } else {
+        alert("Erro: "+res.error);
+      }
+    }
+
+    setLoading(false);
+  }
+
   return (
     <Container>
-      <Text>Search</Text>
+      <SearchArea>
+        <SearchInput
+          placeholder="Digite o nome do barbeiro"
+          placeholderTextColor="#ffffff"
+          value={searchText}
+          onChangeText={t=>setSearchText(t)}
+          onEndEditing={searchBarbers}
+          returnKeyType="search"
+          autoFocus
+          selectTextOnFocus
+        />
+      </SearchArea>
+
+      <Scroller>
+        {loading && 
+          <LoadingIcon size="large" color="#000000" />
+        }
+        {emptyList && 
+          <EmptyWarning>Não achamos barbeiros com o nome "{searchText}"</EmptyWarning>
+        }
+        <ListArea>
+          {list.map((item, k)=> (
+            <BarberItem key={k} data={item} />
+          ))}
+        </ListArea>
+      </Scroller>
+
     </Container>
   );
 }
